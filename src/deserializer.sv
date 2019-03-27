08790f06714dacb3a2503c224f662d2de9a2ca46
Chepelev Anton <ChepelevAnton@local.com>
2019-03-27 14:44:03 +0300

    Separating always blocks

--- src/deserializer.sv
+++ src/deserializer.sv
@@ -17,22 +17,30 @@ always_ff @( posedge clk_i )
     if( srst_i )
       begin
         cntr             <= '0;
-        deser_data_o     <= '0;
         deser_data_val_o <= '0;
       end
     else if( data_val_i )
       begin
-        cntr               <= cntr + 1'b1;
-        deser_data_o[cntr] <= data_i;
-        if( cntr == 'b1111 )          
-          deser_data_val_o <= 1'b1;          
+        cntr <= cntr + 1'b1;
+        if( cntr == 4'd15 )
+          deser_data_val_o <= 1'b1;
         else
           deser_data_val_o <= 1'b0;
       end
     else
       begin
         deser_data_val_o <= 1'b0;
-      end
+      end          
+
+  end  
+
+
+always_ff @( posedge clk_i )
+  begin
+    if( srst_i )      
+        deser_data_o     <= '0;  
+    else if( data_val_i )       
+        deser_data_o[cntr] <= data_i; 
   end
 
 
